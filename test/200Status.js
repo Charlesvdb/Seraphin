d85f2e69f940f9805ba08bc1ef9612d28614668f
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-07-18 18:00:41 +0200

    200 message younger than 25 and eligible

--- test/200Status.js
+++ test/200Status.js
@@ -9,3 +9,23 @@ const pathUrl = "/api/v1/quote/car-insurance";
 
 chai.use(require("chai-http"));
 
+describe ('200 message status', () => {
+    it('should return 200 status message if person younger than 25 years and eligible', ()=>{
+      chai.request('http://localhost:8080')
+      .post(url)
+      .set('Accept', 'application/json')
+      .send({
+              "car_value": 20000.00,
+              "driver_birthdate": "11/01/2013"
+            })
+      .end((err, res) => {
+        assert.equal(res.body.success, true)
+        assert.equal(res.body.message, "quote successfully computed")
+        assert.equal(res.body.data.eligible, true)
+        assert.equal(res.body.data.premiums.civil_liability, 1000)
+        assert.equal(res.body.data.premiums.omnium, res.request._data.car_value * 0.03)
+      })
+    });
+
+})
+
