e058cd44ae42fb2027c341c82d05c0cefb381687
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-07-18 18:10:46 +0200

    200 message when not eligible person

--- test/200Status.js
+++ test/200Status.js
@@ -10,6 +10,23 @@ const pathUrl = "/api/v1/quote/car-insurance";
 chai.use(require("chai-http"));
 
 describe ('200 message status', () => {
+    it('should return the 200 status message if the person is not eligible', ()=>{
+        chai.request('http://localhost:8080')
+        .post(url)
+        .set('Accept', 'application/json')
+        .send({
+                "car_value": 22000.00,
+                "driver_birthdate": "17/11/2010"
+              })
+        .end((err, res) => {
+          assert.equal(res.body.success, true)
+          assert.equal(res.body.message, "quote successfully computed")
+          assert.equal(res.body.data.eligible, false)
+          assert.equal(res.body.data.premiums, null)
+        })
+      });
+
+
     it('should return 200 status message if person up to 25 years and eligible', ()=>{
       chai.request('http://localhost:8080')
       .post(url)
