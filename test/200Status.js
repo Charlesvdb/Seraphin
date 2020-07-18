e88828032c83d1058dd8295cccdac957e1ad3973
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-07-18 18:08:24 +0200

    200 message if older than 25 and eligible

--- test/200Status.js
+++ test/200Status.js
@@ -10,12 +10,12 @@ const pathUrl = "/api/v1/quote/car-insurance";
 chai.use(require("chai-http"));
 
 describe ('200 message status', () => {
-    it('should return 200 status message if person younger than 25 years and eligible', ()=>{
+    it('should return 200 status message if person up to 25 years and eligible', ()=>{
       chai.request('http://localhost:8080')
       .post(url)
       .set('Accept', 'application/json')
       .send({
-              "car_value": 20000.00,
+              "car_value": 13000.00,
               "driver_birthdate": "11/01/2013"
             })
       .end((err, res) => {
@@ -26,6 +26,23 @@ describe ('200 message status', () => {
         assert.equal(res.body.data.premiums.omnium, res.request._data.car_value * 0.03)
       })
     });
+
+    it('should return 200 status message if person older than 25 years and eligible', ()=>{
+        chai.request('http://localhost:8080')
+        .post(url)
+        .set('Accept', 'application/json')
+        .send({
+                "car_value": 10000.00,
+                "driver_birthdate": "12/02/1988"
+              })
+        .end((err, res) => {
+          assert.equal(res.body.success, true)
+          assert.equal(res.body.message, "quote successfully computed")
+          assert.equal(res.body.data.eligible, true)
+          assert.equal(res.body.data.premiums.civil_liability, 1000)
+          assert.equal(res.body.data.premiums.omnium, res.request._data.car_value * 0.03)
+        })
+      });
 
 })
 
