cfc8780dc9ab1402c1776c92fc332006bc6d5381
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-07-18 16:55:04 +0200

    carvalue null

--- test/400ErrorStatus.js
+++ test/400ErrorStatus.js
@@ -9,4 +9,20 @@ const pathUrl = "/api/v1/quote/car-insurance";
 
 chai.use(require("chai-http"));
 
+describe ('400 error status', () => {
+    it('should return status 400 if car_value is not given', ()=>{
+      chai.request('http://localhost:8080')
+      .post(url)
+      .set('Accept', 'application/json')
+      .send({
+              "car_value": null,
+              "driver_birthdate": "15/10/1990"
+            })
+      .end((err, res) => {
+        assert.equal(res.status, 400, "status 400")
+      })
+    });
 
+
+
+})    
