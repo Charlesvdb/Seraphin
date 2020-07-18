712dd41e57a1725a1e4e5cedc64fdb6d19023647
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-07-18 16:58:25 +0200

    400 error if driver_birthdate is null

--- test/400ErrorStatus.js
+++ test/400ErrorStatus.js
@@ -10,19 +10,31 @@ const pathUrl = "/api/v1/quote/car-insurance";
 chai.use(require("chai-http"));
 
 describe ('400 error status', () => {
-    it('should return status 400 if car_value is not given', ()=>{
+    it('should return status 400 error if car_value is not given', ()=>{
       chai.request('http://localhost:8080')
-      .post(url)
+      .post(pathUrl)
       .set('Accept', 'application/json')
       .send({
               "car_value": null,
-              "driver_birthdate": "15/10/1990"
+              "driver_birthdate": "12/08/1993"
             })
       .end((err, res) => {
         assert.equal(res.status, 400, "status 400")
       })
     });
 
+    it('should return status 400 erro if driver_birthdate is not given', ()=>{
+        chai.request('http://localhost:8080')
+        .post(pathUrl)
+        .set('Accept', 'application/json')
+        .send({
+                "car_value": 123455.00,
+                "driver_birthdate": "null"
+              })
+        .end((err, res) => {
+          assert.equal(res.status, 400, "status 400")
+        })
+    });
 
 
 })    
