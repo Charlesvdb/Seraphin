31b2a0d6829133dc10af42a6ed16b25091c73e41
Charles Van den Bergh <vandenberghcharles@gmail.com>
2020-07-18 16:26:21 +0200

    eligible younger than 18 years old

--- test/ageChecks.js
+++ test/ageChecks.js
@@ -7,19 +7,35 @@ const url = "/api/v1/quote/car-insurance";
 
 chai.use(require("chai-http"));
 
-describe ('is driver eligible for an insurance premium?', () => {
-    it('it should return true if the driver is at least 18Y old', () =>{
+describe ('is person eligible for an insurance premium?', () => {
+    it('should return TRUE if the person is at least 18Y old', () =>{
       chai.request('http://localhost:8080')
       .post(url)
       .set('Accept', 'application/json')
       .send({
               "car_value": 15000.00,
-              "driver_birthdate": "15/10/1985"
+              "driver_birthdate": "23/01/1985"
             })
       .end((err, res) => {
         assert.equal(res.status, 200, "Status OK 200")
         assert.equal(res.body.data.eligible, true)
     })
 });
+
+it('should return FALSE if the driver is younger than 18Y old', ()=>{
+    chai.request('http://localhost:8080')
+    .post(url)
+    .set('Accept', 'application/json')
+    .send({
+            "car_value": 13000.00,
+            "driver_birthdate": "13/02/2012"
+          })
+    .end((err, res) => {
+      assert.equal(res.status, 200, "Status OK 200")
+      assert.equal(res.body.data.eligible, false)
+    })
+  });
+
+})
 
 
